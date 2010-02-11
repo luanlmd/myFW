44a4e572d81bf6eb42bf5a8bf3f4acbc87043020
Luan Almeida <luanlmd@gmail.com>
2010-02-11 09:31:12 -0300

    Now I had to use .gitignore

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -3,7 +3,7 @@ class IndexController extends Controller
 {
 	function index()
 	{
-		// Set the var to be used in the view file
+		// Set a var to be used in the view file
 		$this->cfile = __FILE__;
 	}
 }
