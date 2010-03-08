337052da6a5b4647dfc8a8097cd4cc113ce24201
Luan Almeida <luanlmd@gmail.com>
2010-03-08 11:13:39 -0300

    Added example of Database connection setup

--- webroot/index.php
+++ webroot/index.php
@@ -8,6 +8,9 @@ require("../classes/App.php");
 // Routes:
 // App::addRoute("^/a/?$","about"); // example.com/a is the same as example.com/about
 
+// Setup Database connection:
+// Database::getInstance()->init('dsn','user','password');
+
 // Run the App. The id parameter will make sure all the Sessions and Cookies use unique keys and become encrypted
 
 App::run("Your Project Name/ID");
